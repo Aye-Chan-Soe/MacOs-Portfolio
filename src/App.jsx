8599c8433696207f87eae73f3e8f1a08f234e7c4
aye-chan-soe <ayechansoe.dev@gmail.com>
2026-07-19 11:28:47 +0000

    implement two more windows - Safari, Resume

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { Navbar, Welcome, Dock } from "@components";
-import { Terminal } from "@windows";
+import { Resume, Safari, Terminal } from "@windows";
 
 import gsap from "gsap"
 import { Draggable } from "gsap/Draggable";
@@ -12,6 +12,8 @@ const App = () => {
       <Welcome/>
       <Dock/>
       <Terminal/>
+      <Safari/>
+      <Resume/>
     </main>
   );
 };
